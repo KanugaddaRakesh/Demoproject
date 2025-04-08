d6b3360dff08d232c5529b5fcc7bff948855f1de
rakesh <rakeshkanugadda@gmail.com>
2025-04-08 12:57:04 +0530

    this is from 1 guy

---
+++ src/test/java/rakesh/gittesting.java
@@ -0,0 +1,12 @@
+package rakesh;
+
+public class gittesting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("git test1");
+		System.out.println("git test1");
+	}
+
+}
